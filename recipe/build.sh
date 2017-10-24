a91d5365e4386e319e6b32e02b4bafa90719abcb
Charles Doutriaux <doutriaux1@llnl.gov>
2017-10-24 08:44:12 -0700

    thx @ocefpaf this should adress your concerns

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
-if [ `uname` == "Linux" ]; then
-    LD_SUFFIX="so"
-elif [ `uname` == "Darwin" ]; then
-    LD_SUFFIX="dylib"
-else
-    LD_SUFFIX="DLL"
-fi
-
 
 # Build static.
 cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
-      -D CURL_INCLUDE_DIR=$PREFIX/include \
-      -D CURL_LIBRARY=$PREFIX/lib/libcurl.$LD_SUFFIX \
       -D CMAKE_INSTALL_LIBDIR:PATH=$PREFIX/lib \
       -D ENABLE_DAP=ON \
       -D ENABLE_HDF4=ON \
@@ -21,6 +11,8 @@ cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
       -D BUILD_UTILITIES=ON \
       -D ENABLE_DOXYGEN=OFF \
       -D ENABLE_LOGGING=ON \
+      -D CURL_INCLUDE_DIR=$PREFIX/include \
+      -D CURL_LIBRARY=$PREFIX/lib/libcurl.${SHLIB_EXT} \
       $SRC_DIR
 make
 # ctest  # Run only for the shared lib build to save time.
@@ -38,6 +30,8 @@ cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
       -D BUILD_UTILITIES=ON \
       -D ENABLE_DOXYGEN=OFF \
       -D ENABLE_LOGGING=ON \
+      -D CURL_INCLUDE_DIR=$PREFIX/include \
+      -D CURL_LIBRARY=$PREFIX/lib/libcurl.${SHLIB_EXT} \
       $SRC_DIR
 make
 ctest
