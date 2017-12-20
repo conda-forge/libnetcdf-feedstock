3fc00b5ae05646a13fff9a372491b2b0ac3ba136
Ray Donnelly <mingw.android@gmail.com>
2017-12-20 16:22:25 +0000

    Fix buffer overrun

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,49 +1,56 @@
 #!/bin/bash
 
-export CFLAGS="$CFLAGS -I$PREFIX/include"
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-
+declare -a CMAKE_PLATFORM_FLAGS
 if [[ ${HOST} =~ .*darwin.* ]]; then
-    CMAKE_TOOLCHAIN_FLAGS=""
+  CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}")
+  export LDFLAGS=${LDFLAGS_CC}
 else
-    CMAKE_TOOLCHAIN_FLAGS=-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake"
+  CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
+fi
+
+if [[ ${DEBUG_C} == yes ]]; then
+  CMAKE_BUILD_TYPE=Debug
+else
+  CMAKE_BUILD_TYPE=Release
 fi
 
 # Build static.
-cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
-      -D CMAKE_INSTALL_LIBDIR:PATH=$PREFIX/lib \
-      -D ENABLE_DAP=ON \
-      -D ENABLE_HDF4=ON \
-      -D ENABLE_NETCDF_4=ON \
-      -D BUILD_SHARED_LIBS=OFF \
-      -D ENABLE_TESTS=OFF \
-      -D BUILD_UTILITIES=ON \
-      -D ENABLE_DOXYGEN=OFF \
-      -D ENABLE_LOGGING=ON \
-      -D CURL_INCLUDE_DIR=$PREFIX/include \
-      -D CURL_LIBRARY=$PREFIX/lib/libcurl${SHLIB_EXT} \
-      $CMAKE_TOOLCHAIN_FLAGS \
-      $SRC_DIR
-make -j$CPU_COUNT
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+      -DCMAKE_INSTALL_LIBDIR:PATH=${PREFIX}/lib \
+      -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} \
+      -DENABLE_DAP=ON \
+      -DENABLE_HDF4=ON \
+      -DENABLE_NETCDF_4=ON \
+      -DBUILD_SHARED_LIBS=OFF \
+      -DENABLE_TESTS=ON \
+      -DBUILD_UTILITIES=ON \
+      -DENABLE_DOXYGEN=OFF \
+      -DENABLE_LOGGING=ON \
+      -DCURL_INCLUDE_DIR=${PREFIX}/include \
+      -DCURL_LIBRARY=${PREFIX}/lib/libcurl${SHLIB_EXT} \
+      ${CMAKE_PLATFORM_FLAGS[@]} \
+      ${SRC_DIR}
+make -j${CPU_COUNT} ${VERBOSE_CM}
 # ctest  # Run only for the shared lib build to save time.
-make install -j$CPU_COUNT
+make install -j${CPU_COUNT}
 make clean
 
 # Build shared.
-cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
-      -D CMAKE_INSTALL_LIBDIR:PATH=$PREFIX/lib \
-      -D ENABLE_DAP=ON \
-      -D ENABLE_HDF4=ON \
-      -D ENABLE_NETCDF_4=ON \
-      -D BUILD_SHARED_LIBS=ON \
-      -D ENABLE_TESTS=OFF \
-      -D BUILD_UTILITIES=ON \
-      -D ENABLE_DOXYGEN=OFF \
-      -D ENABLE_LOGGING=ON \
-      -D CURL_INCLUDE_DIR=$PREFIX/include \
-      -D CURL_LIBRARY=$PREFIX/lib/libcurl${SHLIB_EXT} \
-      $CMAKE_TOOLCHAIN_FLAGS \
-      $SRC_DIR
-make -j$CPU_COUNT
-make install -j$CPU_COUNT
-ctest
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+      -DCMAKE_INSTALL_LIBDIR:PATH=${PREFIX}/lib \
+      -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} \
+      -DENABLE_DAP=ON \
+      -DENABLE_HDF4=ON \
+      -DENABLE_NETCDF_4=ON \
+      -DBUILD_SHARED_LIBS=ON \
+      -DENABLE_TESTS=ON \
+      -DBUILD_UTILITIES=ON \
+      -DENABLE_DOXYGEN=OFF \
+      -DENABLE_LOGGING=ON \
+      -DCURL_INCLUDE_DIR=${PREFIX}/include \
+      -DCURL_LIBRARY=${PREFIX}/lib/libcurl${SHLIB_EXT} \
+      ${CMAKE_PLATFORM_FLAGS[@]} \
+      ${SRC_DIR}
+make -j${CPU_COUNT} ${VERBOSE_CM}
+make install -j${CPU_COUNT}
+ctest --extra-verbose --debug
