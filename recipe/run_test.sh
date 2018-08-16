93f5464103f6237a6490d190071f34dc32cd9d94
Filipe Fernandes <ocefpaf@gmail.com>
2018-08-16 11:08:38 -0300

    comment on the no-s

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,6 +1,3 @@
-nc-config --has-c++      | grep -q no
-nc-config --has-c++4     | grep -q no
-nc-config --has-fortran  | grep -q no
 nc-config --has-dap      | grep -q yes
 nc-config --has-dap2     | grep -q yes
 nc-config --has-dap4     | grep -q yes
@@ -9,7 +6,18 @@ nc-config --has-nc4      | grep -q yes
 nc-config --has-hdf5     | grep -q yes
 nc-config --has-hdf4     | grep -q yes
 nc-config --has-logging  | grep -q yes
-nc-config --has-pnetcdf  | grep -q no
-nc-config --has-szlib    | grep -q no
 nc-config --has-cdf5     | grep -q yes
-nc-config --has-parallel | grep -q no
+
+# C++ and Fortran are now separate packages (netcdf-cxx4 and netcdf-fortran)
+# nc-config --has-c++      | grep -q no
+# nc-config --has-c++4     | grep -q no
+# nc-config --has-fortran  | grep -q no
+
+# Parallel is the one we would like to have.
+# nc-config --has-parallel | grep -q no
+
+# Not sure if people still uses pnetcdf.
+# nc-config --has-pnetcdf  | grep -q no
+
+# We cannot package szip due to its license
+# nc-config --has-szlib    | grep -q no
