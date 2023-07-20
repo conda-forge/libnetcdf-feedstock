7e3bc3d2d81624b8bab37a7f910d6fedd023275d
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2023-07-20 17:23:05 +0200

    Always raise an exception to test test running

--- recipe/test_netcdf.py
+++ recipe/test_netcdf.py
@@ -4,3 +4,4 @@ from netCDF4 import Dataset
 def test_netcdf():
     nc = Dataset("test_netcdf4_python.nc", mode="w")
     nc.close()
+    raise RuntimeError("Something is indeed going wrong (or right)")
