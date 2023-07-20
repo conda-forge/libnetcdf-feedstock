16281d0f7bdc444452eb0bfed2d743d17422ab48
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2023-07-20 15:38:55 +0200

    Simplify extra test

--- recipe/test_netcdf.py
+++ recipe/test_netcdf.py
@@ -1,14 +1,6 @@
-import xarray as xr
+from netCDF4 import Dataset
 
 
-def test_netcdf():
-    fk = "test.nc"
-    ds = xr.Dataset(
-        coords={"nx": [0], "ny": [0]},
-        attrs={
-            "source": "satpy unit test",
-            "time_coverage_start": "0001-01-01T00:00:00Z",
-            "time_coverage_end": "0001-01-01T01:00:00Z",
-        }
-    )
-    ds.to_netcdf(fk)
+def test_netcdf4():
+    nc = Dataset("test_netcdf4_python.nc", mode="w")
+    nc.close()
